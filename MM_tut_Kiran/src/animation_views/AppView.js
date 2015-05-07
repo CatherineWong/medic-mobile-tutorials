6abfd60fcce37b8b456a1de980961556ddd746de
CatherineWong <wong.catherine.mail@gmail.com>
2015-05-07 01:01:57 -0700

    Revert 38baec6..ad2165c
    This rolls back to commit 38baec6a48b2a4038c51a071dcb7097a8a0f9314.

--- MM_tut_Kiran/src/animation_views/AppView.js
+++ MM_tut_Kiran/src/animation_views/AppView.js
@@ -8,18 +8,13 @@ define(function(require, exports, module) {
     var PageView = require('animation_views/PageView');
     var MenuView = require('animation_views/MenuView');
     var StripData = require('Data/StripData');
-    var CaptionData = require('Data/CaptionData');
-
 
     function AppView() {
         View.apply(this, arguments);
 
         this.menuToggle = false;
-
         _createPageView.call(this);
-
         _createMenuView.call(this);
-        
         _setListeners.call(this);
     }
 
@@ -29,7 +24,7 @@ define(function(require, exports, module) {
     AppView.DEFAULT_OPTIONS = {};
 
     function _createPageView() {
-        this.pageView = new PageView({ captionData: CaptionData });
+        this.pageView = new PageView();
         this.pageModifier = new StateModifier();
 
         this.add(this.pageModifier).add(this.pageView);
@@ -38,16 +33,14 @@ define(function(require, exports, module) {
     function _createMenuView() {
         this.menuView = new MenuView({ stripData: StripData });
 
-        this.anotherModifier = new StateModifier ({
+        var anotherModifier = new StateModifier ({
             transform: Transform.translate(0, 0, 0.2)   //use this z axis to bring in front of surface
         });
 
         this.menuModifier = new StateModifier();
 
-        this.add(this.menuModifier).add(this.anotherModifier).add(this.menuView);
+        this.add(this.menuModifier).add(anotherModifier).add(this.menuView);
     }
-
-    
 
     function _setListeners() {
         this.pageView.on('menuToggle', function(){
@@ -57,42 +50,31 @@ define(function(require, exports, module) {
 
     function _setListeners() {
         this.pageView.on('menuToggle', this.toggleMenu.bind(this));
-        this.pageView.on('next', this.nextSlide.bind(this));
     }
 
     AppView.prototype.toggleMenu = function() {
         if(this.menuToggle) {
-            this.popUp();
+            this.slideLeft();
         } else {
-            this.popDown();
+            this.slideRight();
         }
         this.menuToggle = !this.menuToggle;
     };
 
-    AppView.prototype.nextSlide = function() {
-
-    }
-
     
 
-    AppView.prototype.popUp = function() {
-        //this.menuModifier.setTransform(Transform.translate(0,1,0.05));
-        this.anotherModifier.setTransform(Transform.translate(0, 0, 0.2), {
-            duration: 300,
-            //curve: 'easeOut'
-        });
-        /*this.menuModifier.setTransform(Transform.translate(0, -200, 0), {
+    AppView.prototype.slideRight = function() {
+        this.menuModifier.setTransform(Transform.translate(0, -200, 0), {
             duration: 300,
             curve: 'easeOut'
-        });*/
+        });
     };
 
-    AppView.prototype.popDown = function() {
-        this.anotherModifier.setTransform(Transform.translate(0, 0, 0));
-        /*this.menuModifier.setTransform(Transform.translate(0, 0, 0), {
+    AppView.prototype.slideLeft = function() {
+        this.menuModifier.setTransform(Transform.translate(0, 0, 0), {
             duration: 300,
             curve: 'easeOut'
-        });*/
+        });
     };
 
     module.exports = AppView;
